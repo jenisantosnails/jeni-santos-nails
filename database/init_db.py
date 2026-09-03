5dd23496450f0b79487dbbf3849780c5a28027df
Jeni Santos Nails <jenisilverr@gmail.com>
2026-09-03 19:52:19 -0300

    Inicializa servicos no banco de producao

--- database/init_db.py
+++ database/init_db.py
@@ -1,4 +1,5 @@
 ﻿from database.database import Base, engine
+from database.seed import seed_services
 
 from models.appointment import Appointment
 from models.client import Client
@@ -11,3 +12,5 @@ print("Criando tabelas do Jeni Santos Nails...")
 Base.metadata.create_all(bind=engine)
 
 print("Banco de dados atualizado com sucesso!")
+
+seed_services()
